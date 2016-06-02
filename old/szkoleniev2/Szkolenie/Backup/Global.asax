2b4870e13720bf7d85b55fd0319c8f4e80943b6a
bitpocket <lematero@gmail.com>
2016-06-03 01:29:32 +0200

    old solution deleted

--- old/szkoleniev2/Szkolenie/Backup/Global.asax
+++
@@ -1,1 +0,0 @@
-﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Opole.Global" Language="C#" %>
--- old/szkoleniev2/archiv/Szkolenie/Misc/UserModel.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Opole.Misc
-{
-    public class UserModel
-    {
-        public int UserId { get; set; }
-        public bool CredentialsCorrect { get; set; }
-        public string Username { get; set; }
-    }
-}
